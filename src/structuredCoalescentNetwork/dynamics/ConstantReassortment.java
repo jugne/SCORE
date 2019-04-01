4bd2216c85c10c2e5bb4e763f3c7337e20576c25
Ugne Jankauskaite <jugne@student.ethz.ch>
2019-04-01 16:36:44 +0200

    More dynamics, not finished

--- src/structuredCoalescentNetwork/dynamics/ConstantReassortment.java
+++ src/structuredCoalescentNetwork/dynamics/ConstantReassortment.java
@@ -13,76 +13,85 @@ public class ConstantReassortment extends Dynamics implements Loggable {
     public Input<RealParameter> reassortmentRates = new Input<>("reassortmentRates", "input of reassortment rates",
 	    Validate.REQUIRED);
 
-    enum DynamicsTypes {
-	constant, bssvs, glm
-    };
+    public Input<Dynamics> structuredCoalescentDynamics = new Input<>("structuredCoalescentDynamics",
+	    "input of dynamics to use for structured coalescent. See MASCOT dynamics package.", Validate.REQUIRED);
 
-    public Input<DynamicsTypes> implementationInput = new Input<>("implementation",
-	    "implementation, one of " + DynamicsTypes.values().toString(), DynamicsTypes.constant);
+    Dynamics scDynamics;
+
+//    enum DynamicsTypes {
+//	constant, bssvs, glm
+//    };
+//
+//    public Input<DynamicsTypes> implementationInput = new Input<>("implementation",
+//	    "implementation, one of " + DynamicsTypes.values().toString(), DynamicsTypes.constant);
 
     @Override
     public void initAndValidate() {
 	super.initAndValidate();
 
+	if (dimensionInput.get() < 1)
+	    dimensionInput.set(getNrTypes());
+	scDynamics = structuredCoalescentDynamics.get();
     }
 
     public double[] getReassortmentRate(int i) {
-	double[] coal = new double[NeInput.get().getDimension()];
-	int c = 0;
-	for (int j = 0; j < NeInput.get().getDimension(); j++) {
-	    reassort[c] = 1 / (ploidyInput.get() * NeInput.get().getArrayValue(j));
-	    c++;
+	if (reassortmentRates.get().getDimension() != dimensionInput.get()) {
+	    System.err.println("Wrong dimension of reassortment rates input. "
+		    + "Reassortment rates for all types set to the value: " + reassortmentRates.get().getArrayValue(0));
+	    reassortmentRates.get().setDimension(dimensionInput.get());
 	}
-	return reassort;
+	double[] reassort = new double[dimensionInput.get()];
+	for (int k = 0; k < dimensionInput.get(); k++) {
+	    reassort[k] = reassortmentRates.get().getArrayValue(k);
+	}
 
+	return reassort;
     }
 
     @Override
     public void close(PrintStream out) {
-	// TODO Auto-generated method stub
-
+	scDynamics.recalculate();
     }
 
     @Override
     public void recalculate() {
-	// TODO Auto-generated method stub
-
+	scDynamics.recalculate();
     }
 
     @Override
     public double getInterval(int i) {
-	// TODO Auto-generated method stub
-	return 0;
+	return scDynamics.getInterval(i);
     }
 
     @Override
     public double[] getIntervals() {
-	// TODO Auto-generated method stub
-	return null;
+	return scDynamics.getIntervals();
     }
 
     @Override
     public boolean intervalIsDirty(int i) {
-	// TODO Auto-generated method stub
-	return false;
+	return scDynamics.intervalIsDirty(i);
     }
 
     @Override
     public double[] getCoalescentRate(int i) {
-	// TODO Auto-generated method stub
-	return null;
+	return scDynamics.getCoalescentRate(i);
     }
 
     @Override
     public double[] getBackwardsMigration(int i) {
-	// TODO Auto-generated method stub
-	return null;
+	return scDynamics.getBackwardsMigration(i);
     }
 
     @Override
     public int getEpochCount() {
-	// TODO Auto-generated method stub
-	return 0;
+	return scDynamics.getEpochCount();
+    }
+
+    @Override
+    public void init(PrintStream out) {
+	scDynamics.init(out);
+
     }
 
 }
