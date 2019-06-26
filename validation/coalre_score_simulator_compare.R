30379c51febc5db5b47a30fe64173e8277130d27
Ugne Jankauskaite <jugne@student.ethz.ch>
2019-06-26 14:10:09 +0200

    Updated plot scripts

--- validation/coalre_score_simulator_compare.R
+++ validation/coalre_score_simulator_compare.R
@@ -26,7 +26,6 @@ doCompare <- function(dsFileName, mcmcFileName) {
     p1 <- p1 + geom_density(data=dt, aes(x=network.height, colour="CoalRe", linetype="Network height")) 
     p1 <- p1 + geom_density(data=dt, aes(x=network.totalLength, colour="CoalRe", linetype="Network length"))
     p1 <- p1 + theme_minimal(base_size = textSize) + labs(colour = "", linetype="", x="Statistic", y="Density")
-    p1 <- p1 + theme_hc() + theme(legend.position = c(0.8,0.72)) + scale_color_calc()
     
     # Network node count plot
     
@@ -46,9 +45,28 @@ doCompare <- function(dsFileName, mcmcFileName) {
     p2 <- ggplot() + geom_point(aes(x=mids, y=density, colour="SCORE simulator"), dt_hs) + geom_line(aes(x=mids, y=density, colour="SCORE simulator"), dt_hs)
     p2 <- p2 + geom_point(aes(x=mids, y=density, colour="CoalRe"), dt_h) + geom_line(aes(x=mids, y=density, colour="CoalRe"), dt_h)
     p2 <- p2 + theme_minimal(base_size = textSize) + labs(colour = "", linetype="", x="Reassortment count", y="Posterior probability")
-    p2 <- p2 + theme_hc() + theme(legend.position = c(0.8,0.72)) + scale_color_calc()
     
-    grid.arrange(p1, p2)
+    H <- c()
+    L <- c()
+    R <- c()
+    l <- length(df$network.height)
+    # For comparison to run faster, calculations need to be done at fewer ponts.
+    # It can be achieved by taking a fraction smaller than 0.001 below.
+    nPoints <- round(l*0.01)
+    by <- as.integer(l/nPoints)
+    print(l)
+    
+    for (i in seq(l, 0, by=-by)){
+        H <- append(H, log(ks.test(df$network.height[1:(l-i)], dfs$network.height)$statistic))
+        L <- append(L, log(ks.test(df$network.totalLength[1:(l-i)], dfs$network.totalLength)$statistic))
+    }
+    
+    p3 <- ggplot() + geom_line(aes(x=seq(1, length(H)), y=H, colour="Network height"), data.frame(H))
+    p3 <- p3 + geom_line(aes(x=seq(1, length(L)), y=L, colour="Network length"), data.frame(L), linetype="dotted")
+    p3 <- p3 + theme_minimal(base_size = textSize) + labs(colour = "", linetype="", x=paste("Number of logged iterations (minus burnin) / ", by), y="Kolmogorov-Smirnov stat. (log scale)")
+    
+    
+    grid.arrange(p1, p2, p3)
     
     dev.off()
 }
--- validation/mtt_score_simulator_compare.R
+++ validation/mtt_score_simulator_compare.R
@@ -23,10 +23,29 @@ doCompare <- function(dsFileName, ds2FileName) {
     p1 <- ggplot() + geom_density(data=dts, aes(x=network.height, colour="SCORE simulator"), size=1)
     p1 <- p1 + geom_density(data=dt, aes(x=tree.height, colour="MTT"), size=1)
     p1 <- p1 + theme_minimal(base_size = textSize) + labs(colour = "", linetype="", x="Height", y="Density")
-    p1 <- p1 + theme_hc() + theme(legend.position = c(0.8,0.75)) + scale_color_calc()
-
+    p1 <- p1 + theme_hc() + theme(legend.position = c(0.8,0.75))
     
-    grid.arrange(p1)
+    H <- c()
+    L <- c()
+    R <- c()
+    l <- length(df$tree.height)
+    # For comparison to run faster, calculations need to be done at fewer ponts.
+    # It can be achieved by taking a fraction smaller than 0.001 below.
+    nPoints <- round(l*0.01)
+    by <- as.integer(l/nPoints)
+    print(l)
+    
+    for (i in seq(l, 0, by=-by)){
+        H <- append(H, log(ks.test(df$tree.height[1:(l-i)], dfs$network.height)$statistic))
+        L <- append(L, log(ks.test(df$tree.length[1:(l-i)], dfs$network.totalLength)$statistic))
+    }
+    
+    p3 <- ggplot() + geom_line(aes(x=seq(1, length(H)), y=H, colour="Tree height"), data.frame(H))
+    p3 <- p3 + geom_line(aes(x=seq(1, length(L)), y=L, colour="Tree length"), data.frame(L), linetype="dotted")
+    p3 <- p3 + theme_minimal(base_size = textSize) + labs(colour = "", linetype="", x=paste("Number of logged iterations (minus burnin) / ", by), y="Kolmogorov-Smirnov stat. (log scale)")
+    
+    
+    grid.arrange(p1, p3)
     
     dev.off()
 }
