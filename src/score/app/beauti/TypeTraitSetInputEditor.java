26f2182a7cfbdce1a3dcf41c997e87933c75121e
jugne <ugne.stolz@bsse.ethz.ch>
2024-10-07 12:16:09 +0200

    further 2.7 migration changes

--- src/score/app/beauti/TypeTraitSetInputEditor.java
+++ src/score/app/beauti/TypeTraitSetInputEditor.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package score.beuti;
+package score.app.beauti;
 
 import beastfx.app.inputeditor.BeautiDoc;
 import beastfx.app.inputeditor.GuessPatternDialog;
